e83f573bf3695bfff60f17be254024a929a22644
keimha <keimham@yahoo.com>
2024-08-29 15:23:09 -0500

    first commit

--- src/main/java/com/kmoore/daikichi_routes/controllers/HomeController.java
+++ src/main/java/com/kmoore/daikichi_routes/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package com.kmoore.daikichi_routes.controllers;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,21 @@ public class HomeController {
     @RequestMapping("/daikichi/tomorrow")
     public String tomorrow() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    // New route for travel
+    @RequestMapping("/daikichi/travel/{city}")
+    public String travel(@PathVariable("city") String city) {
+        return "Congratulations! You will soon travel to " + city + "!";
+    }
+
+    // New route for lotto
+    @RequestMapping("/daikichi/lotto/{number}")
+    public String lotto(@PathVariable("number") int number) {
+        if (number % 2 == 0) {
+            return "You will take a grand journey in the near future, but be weary of tempting offers.";
+        } else {
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        }
     }
 }
